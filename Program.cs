43afd04bb6af2772e879b9941859699786da740c
sshwayat <sshwayat@INP-SALOTOUM>
2026-03-17 10:22:14 +0300

    as services

--- Program.cs
+++ Program.cs
@@ -1,6 +1,7 @@
 using AnnouncmentHub.Data;
 using AnnouncmentHub.Models;
 using AnnouncmentHub.Service;
+using AnnouncmentHub.Services;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 
@@ -40,9 +41,17 @@ builder.Services.AddRazorPages();
 // Services
 builder.Services.AddScoped<AnnouncementRepository>();
 builder.Services.AddScoped<BreadcrumbService>();
+// Register the class without interface
+builder.Services.AddTransient<UserAndRoleDataInitializer>();
 
 var app = builder.Build();
 
+// Initialize data
+using (var scope = app.Services.CreateScope())
+{
+    var initializer = scope.ServiceProvider.GetRequiredService<UserAndRoleDataInitializer>();
+    await initializer.SeedDataAsync(); // or your method name
+}
 if (app.Environment.IsDevelopment())
 {
     app.UseMigrationsEndPoint();
@@ -72,77 +81,3 @@ app.MapControllerRoute(
 app.MapRazorPages();
 
 app.Run();
-//using AnnouncmentHub.Data;
-//using AnnouncmentHub.Models;
-//using AnnouncmentHub.Service;
-//using Microsoft.AspNetCore.Identity;
-//using Microsoft.EntityFrameworkCore;
-
-//var builder = WebApplication.CreateBuilder(args);
-
-//// Add services to the container.
-////var connectionString = builder.Configuration.GetConnectionString("DefaultConnection") ?? throw new InvalidOperationException("Connection string 'DefaultConnection' not found.");
-////builder.Services.AddDbContext<ApplicationDbContext>(options =>
-////    options.UseSqlServer(connectionString));
-
-//builder.Services.AddDatabaseDeveloperPageExceptionFilter();
-
-//builder.Services
-//    .AddIdentity<ApplicationUser, ApplicationRole>(options =>
-//    {
-//        options.SignIn.RequireConfirmedAccount = false;
-
-//        options.Password.RequiredLength = 6;
-//        options.Password.RequireNonAlphanumeric = false;
-//        options.Password.RequireUppercase = false;
-//    })
-//    .AddEntityFrameworkStores<ApplicationDbContext>()
-//    .AddDefaultTokenProviders();
-
-//builder.Services.AddControllersWithViews();
-//builder.Services.AddRazorPages();
-
-
-//// Register Services
-//builder.Services.AddScoped<AnnouncementRepository>();
-
-//builder.Services.AddScoped<BreadcrumbService>();
-
-//var app = builder.Build();
-
-//// Configure the HTTP request pipeline.
-//if (app.Environment.IsDevelopment())
-//{
-//    app.UseMigrationsEndPoint();
-//}
-//else
-//{
-//    app.UseExceptionHandler("/Home/Error");
-//    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-//    app.UseHsts();
-//}
-
-//app.UseHttpsRedirection();
-//app.UseStaticFiles();
-
-//app.UseRouting();
-
-//app.UseAuthentication();  
-
-//app.UseAuthorization();
-
-////app.MapControllerRoute(
-////name: "default",
-////pattern: "{controller=Home}/{action=Index}/{id?}");
-
-//app.MapControllerRoute(
-//    name: "areas",
-//    pattern: "{area:exists}/{controller=Dashboard}/{action=Index}/{id?}");
-
-//app.MapControllerRoute(
-//    name: "default",
-//    pattern: "{controller=Hub}/{action=Index}/{id?}");
-
-//app.MapRazorPages();
-
-//app.Run();
